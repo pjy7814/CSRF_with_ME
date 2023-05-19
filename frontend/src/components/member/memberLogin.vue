f30b553095a091d9483845b963068eb26914a4d4
김수환 <suhwan2004@gmail.com>
2023-05-19 14:41:29 +0900

    fix : user로 된 keyword들을 member로 변경

--- frontend/src/components/member/memberLogin.vue
+++ frontend/src/components/member/memberLogin.vue
@@ -10,28 +10,32 @@
       <b-col cols="8">
         <b-card class="text-center mt-3" style="max-width: 40rem" align="left">
           <b-form class="text-left">
-            <b-alert show variant="danger" v-if="isLoginError">아이디 또는 비밀번호를 확인하세요.</b-alert>
-            <b-form-group label="아이디:" label-for="userid">
+            <b-alert show variant="danger" v-if="isLoginError"
+              >아이디 또는 비밀번호를 확인하세요.</b-alert
+            >
+            <b-form-group label="아이디:" label-for="memberId">
               <b-form-input
-                id="userid"
-                v-model="user.userid"
+                id="memberId"
+                v-model="member.memberId"
                 required
                 placeholder="아이디 입력...."
                 @keyup.enter="confirm"
               ></b-form-input>
             </b-form-group>
-            <b-form-group label="비밀번호:" label-for="userpwd">
+            <b-form-group label="비밀번호:" label-for="memberPassword">
               <b-form-input
                 type="password"
-                id="userpwd"
-                v-model="user.userpwd"
+                id="memberPassword"
+                v-model="member.memberPassword"
                 required
                 placeholder="비밀번호 입력...."
                 @keyup.enter="confirm"
               ></b-form-input>
             </b-form-group>
             <b-button type="button" variant="primary" class="m-1" @click="confirm">로그인</b-button>
-            <b-button type="button" variant="success" class="m-1" @click="movePage">회원가입</b-button>
+            <b-button type="button" variant="success" class="m-1" @click="movePage"
+              >회원가입</b-button
+            >
           </b-form>
         </b-card>
       </b-col>
@@ -46,27 +50,27 @@ import { mapState, mapActions } from "vuex";
 const memberStore = "memberStore";
 
 export default {
-  name: "UserLogin",
+  name: "MemberLogin",
   data() {
     return {
       // isLoginError: false,
-      user: {
-        userid: null,
-        userpwd: null,
+      member: {
+        memberId: null,
+        memberPassword: null,
       },
     };
   },
   computed: {
-    ...mapState(memberStore, ["isLogin", "isLoginError", "userInfo"]),
+    ...mapState(memberStore, ["isLogin", "isLoginError", "memberInfo"]),
   },
   methods: {
-    ...mapActions(memberStore, ["userConfirm", "getUserInfo"]),
+    ...mapActions(memberStore, ["memberConfirm", "getMemberInfo"]),
     async confirm() {
-      await this.userConfirm(this.user);
+      await this.memberConfirm(this.member);
       let token = sessionStorage.getItem("access-token");
       // console.log("1. confirm() token >> " + token);
       if (this.isLogin) {
-        await this.getUserInfo(token);
+        await this.getMemberInfo(token);
         // console.log("4. confirm() userInfo :: ", this.userInfo);
         this.$router.push({ name: "main" });
       }
