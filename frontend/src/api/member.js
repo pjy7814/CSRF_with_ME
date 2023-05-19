c5d2f13831b10cd2b8cf08c853ff5aecb5acfbb6
김수환 <suhwan2004@gmail.com>
2023-05-19 14:42:49 +0900

    fix : member로 키워드 수정 이후, login, logout의 기능 오작동 수정

--- frontend/src/api/member.js
+++ frontend/src/api/member.js
@@ -2,22 +2,22 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
-async function login(user, success, fail) {
-  await api.post(`/user/login`, JSON.stringify(user)).then(success).catch(fail);
+async function login(member, success, fail) {
+  await api.post(`/member/login`, JSON.stringify(member)).then(success).catch(fail);
 }
 
-async function findById(userid, success, fail) {
+async function findById(memberId, success, fail) {
   api.defaults.headers["access-token"] = sessionStorage.getItem("access-token");
-  await api.get(`/user/info/${userid}`).then(success).catch(fail);
+  await api.get(`/member/info/${memberId}`).then(success).catch(fail);
 }
 
-async function tokenRegeneration(user, success, fail) {
+async function tokenRegeneration(member, success, fail) {
   api.defaults.headers["refresh-token"] = sessionStorage.getItem("refresh-token"); //axios header에 refresh-token 셋팅
-  await api.post(`/user/refresh`, user).then(success).catch(fail);
+  await api.post(`/member/refresh`, member).then(success).catch(fail);
 }
 
-async function logout(userid, success, fail) {
-  await api.get(`/user/logout/${userid}`).then(success).catch(fail);
+async function logout(memberId, success, fail) {
+  await api.get(`/member/logout/${memberId}`).then(success).catch(fail);
 }
 
 export { login, findById, tokenRegeneration, logout };
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,17 +8,17 @@ import store from "@/store";
 Vue.use(VueRouter);
 
 // https://v3.router.vuejs.org/kr/guide/advanced/navigation-guards.html
-const onlyAuthUser = async (to, from, next) => {
-  const checkUserInfo = store.getters["memberStore/checkUserInfo"];
+const onlyAuthMember = async (to, from, next) => {
+  const checkMemberInfo = store.getters["memberStore/checkMemberInfo"];
   const checkToken = store.getters["memberStore/checkToken"];
   let token = sessionStorage.getItem("access-token");
-  console.log("로그인 처리 전", checkUserInfo, token);
+  console.log("로그인 처리 전", checkMemberInfo, token);
 
-  if (checkUserInfo != null && token) {
+  if (checkMemberInfo != null && token) {
     console.log("토큰 유효성 체크하러 가자!!!!");
-    await store.dispatch("memberStore/getUserInfo", token);
+    await store.dispatch("memberStore/getMemberInfo", token);
   }
-  if (!checkToken || checkUserInfo === null) {
+  if (!checkToken || checkMemberInfo === null) {
     alert("로그인이 필요한 페이지입니다..");
     // next({ name: "login" });
     router.push({ name: "login" });
@@ -40,136 +40,101 @@ const routes = [
     component: AppDestinationInfo,
   },
   {
-    path: "/user",
-    name: "user",
-    component: () => import(/* webpackChunkName: "user" */ "@/views/AppUser"),
+    path: "/member",
+    name: "member",
+    component: () => import(/* webpackChunkName: "member" */ "@/views/AppMember"),
     children: [
       {
         path: "join",
         name: "join",
         component: () =>
-          import(
-            /* webpackChunkName: "user" */ "@/components/user/UserRegister"
-          ),
+          import(/* webpackChunkName: "member" */ "@/components/member/memberRegister"),
       },
       {
         path: "login",
         name: "login",
-        component: () =>
-          import(/* webpackChunkName: "user" */ "@/components/user/UserLogin"),
+        component: () => import(/* webpackChunkName: "member" */ "@/components/member/memberLogin"),
       },
       {
         path: "mypage",
         name: "mypage",
-        beforeEnter: onlyAuthUser,
+        beforeEnter: onlyAuthMember,
         component: () =>
-          import(/* webpackChunkName: "user" */ "@/components/user/UserMyPage"),
+          import(/* webpackChunkName: "member" */ "@/components/member/memberMyPage"),
       },
     ],
   },
   {
     path: "/noticeboard",
     name: "noticeboard",
-    component: () =>
-      import(/* webpackChunkName: "board" */ "@/views/AppNoticeBoard"),
+    component: () => import(/* webpackChunkName: "board" */ "@/views/AppNoticeBoard"),
     redirect: "/noticeboard/list",
     children: [
       {
         path: "list",
         name: "noticeboardlist",
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardList"
-          ),
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardList"),
       },
       {
         path: "write",
         name: "noticeboardwrite",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardWrite"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardWrite"),
       },
       {
         path: "view/:articleno",
         name: "noticeboardview",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardView"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardView"),
       },
       {
         path: "modify",
         name: "noticeboardmodify",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardModify"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardModify"),
       },
       {
         path: "delete/:articleno",
         name: "noticeboarddelete",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardDelete"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardDelete"),
       },
     ],
   },
   {
     path: "/shareboard",
     name: "shareboard",
-    component: () =>
-      import(/* webpackChunkName: "board" */ "@/views/AppShareBoard"),
+    component: () => import(/* webpackChunkName: "board" */ "@/views/AppShareBoard"),
     redirect: "/shareboard/list",
     children: [
       {
         path: "list",
         name: "shareboardlist",
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardList"
-          ),
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardList"),
       },
       {
         path: "write",
         name: "shareboardwrite",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardWrite"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardWrite"),
       },
       {
         path: "view/:articleno",
         name: "shareboardview",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardView"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardView"),
       },
       {
         path: "modify",
         name: "shareboardmodify",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardModify"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardModify"),
       },
       {
         path: "delete/:articleno",
         name: "shareboarddelete",
-        beforeEnter: onlyAuthUser,
-        component: () =>
-          import(
-            /* webpackChunkName: "board" */ "@/components/board/BoardDelete"
-          ),
+        beforeEnter: onlyAuthMember,
+        component: () => import(/* webpackChunkName: "board" */ "@/components/board/BoardDelete"),
       },
     ],
   },
