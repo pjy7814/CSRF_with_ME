d7981e5e39434f320859c1722f0b24723d3e8bf9
suhwan2004 <suhwan2004@gmail.com>
2023-05-22 01:50:16 +0900

    feature : memberStore내에 로그인 로직에서, 2xx이외에 전송되는 status code에 대한 대응 코드 작성(리펙토링 필요)

--- frontend/src/store/modules/memberStore.js
+++ frontend/src/store/modules/memberStore.js
@@ -39,10 +39,8 @@ const memberStore = {
       await login(
         member,
         ({ data }) => {
-          console.log(data);
           if (data.message === "success") {
             let accessToken = data["access-token"];
-            // console.log("login success token created!!!! >> ", accessToken, refreshToken);
             commit("SET_IS_LOGIN", true);
             commit("SET_IS_LOGIN_ERROR", false);
             commit("SET_IS_VALID_TOKEN", true);
@@ -54,6 +52,42 @@ const memberStore = {
           }
         },
         (error) => {
+          /*
+          error code 400 : Json parser error => 바디에 값이 아이디와 패스워드가 없음. => 해킹 의심. 일단은, 계속 로그인 시키도록 진행.
+          error code 500 : internal server error => 에러 페이지로 보내주는게 맞음.
+          */
+          switch (error.response.status) {
+            case 400:
+              //Response.body의 에러 시에 js 코드를 고쳐서 submit 버튼을 시도했음. 로그인 페이지에 경고 메시지를 띄움.
+              commit("SET_IS_LOGIN", false);
+              commit("SET_IS_LOGIN_ERROR", true);
+              commit("SET_IS_VALID_TOKEN", false);
+              break;
+            case 500:
+              // server 에러의 경우, 점검 중 또는 현재 서버에 이상이 있음. 에러 페이지로 이동.
+              commit("SET_IS_LOGIN", false);
+              commit("SET_IS_LOGIN_ERROR", false);
+              commit("SET_IS_VALID_TOKEN", false);
+              this.$router.push({
+                name: "error",
+                params: {
+                  msg: "서버 에러입니다! 관리자에게 문의해주세요.",
+                },
+              });
+              break;
+            default:
+              //범위에 벗어나는 에러일 경우, 의도에 벗어난 에러임. 에러 페이지 이동 및 안내 문구 띄우기
+              commit("SET_IS_LOGIN", false);
+              commit("SET_IS_LOGIN_ERROR", false);
+              commit("SET_IS_VALID_TOKEN", false);
+              this.$router.push({
+                name: "error",
+                params: {
+                  msg: "비 정상적인 접근입니다! 사이트를 정상적으로 이용해주세요!",
+                },
+              });
+              break;
+          }
           console.log(error);
         }
       );
@@ -82,7 +116,10 @@ const memberStore = {
       );
     },
     async tokenRegeneration({ commit, state }) {
-      console.log("토큰 재발급 >> 기존 토큰 정보 : {}", sessionStorage.getItem("access-token"));
+      console.log(
+        "토큰 재발급 >> 기존 토큰 정보 : {}",
+        sessionStorage.getItem("access-token")
+      );
       await tokenRegeneration(
         JSON.stringify(state.memberInfo),
         ({ data }) => {
