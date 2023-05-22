ce5fa8da5d4f8b7f9eb60d9619d038a03f440f10
suhwan2004 <suhwan2004@gmail.com>
2023-05-23 03:09:20 +0900

    feature : BoardInputItem.vue에서 공유 여행지를 모달창 내에 kakaoMap Component를 통해 선택할 수 있도록 기능 구현

--- frontend/src/components/board/item/BoardInputItem.vue
+++ frontend/src/components/board/item/BoardInputItem.vue
@@ -5,12 +5,12 @@
         <b-form-group
           label-cols="2"
           content-cols="10"
-          label="제목:"
-          label-for="subject"
+          label="제목 :"
+          label-for="title"
         >
           <b-form-input
-            id="subject"
-            v-model="article.subject"
+            id="title"
+            v-model="boardTitle.value"
             type="text"
             required
             placeholder="제목 입력..."
@@ -19,11 +19,12 @@
         <b-form-group
           label-cols="2"
           content-cols="10"
-          label="이미지 등록"
-          label-for="input-horizontal"
+          label="이미지 등록 :"
+          label-for="uploadImage"
         >
           <b-form-file
-            v-model="selectedImg"
+            id="uploadImage"
+            v-model="boardImageUrl.value"
             variant="success"
             multiple
             accept=".jpg, .jpeg, .png, .gif"
@@ -32,10 +33,61 @@
           ></b-form-file>
         </b-form-group>
 
+        <b-form-group
+          label-cols="2"
+          content-cols="10"
+          label="여행지 입력 :"
+          label-for="uploadAttraction"
+        >
+          <b-input-group>
+            <b-form-input
+              id="uploadAttraction"
+              type="text"
+              required
+              readonly
+              v-model="boardAttractionInfo.value.title"
+              placeholder="공유할 여행지를 등록해주세요!"
+            ></b-form-input>
+            <b-button @click="openModal()">등록하기</b-button>
+          </b-input-group>
+        </b-form-group>
+        <b-modal v-model="isModalOpen" @hide="closeModal()" size="lg" centered>
+          <template #modal-title> 공유 여행지 등록하기 </template>
+          <template #default>
+            <AppDestinationInfo
+              startPoint="boardModal"
+              @registModalAttraction="registModalAttraction"
+            ></AppDestinationInfo>
+          </template>
+          <template #modal-footer>
+            <b-form-group
+              label-cols="2"
+              content-cols="10"
+              label="현재 선택된 여행지 :"
+              label-for="selectedModalAttraction"
+            >
+              <b-form-input
+                id="selectedModalAttraction"
+                v-model="selectedModalAttraction.title"
+                type="text"
+                required
+                readonly
+                placeholder="공유할 여행지 한 곳을 등록해주세요!"
+              ></b-form-input>
+            </b-form-group>
+            <b-button
+              @click="registAttraction()"
+              :disabled="isValidModalAttraction"
+              >등록</b-button
+            >
+            <b-button @click="closeModal()">닫기</b-button>
+          </template>
+        </b-modal>
+
         <b-form-group id="content-group" label="내용:" label-for="content">
           <b-form-textarea
             id="content"
-            v-model="article.content"
+            v-model="boardContent.value"
             placeholder="내용 입력..."
             rows="10"
             max-rows="15"
@@ -60,16 +112,17 @@
 
 <script>
 import { writeArticle, modifyArticle, getArticle } from "@/api/board";
+import AppDestinationInfo from "@/views/AppDestinationInfo.vue";
 
 /*
  fix me!
   - 토큰 만료 시, interceptor에서 토큰 초기화 및 로그아웃 response를 반환해줘야 한다.
-  - 글 작성 시, request할 시에 서버에서 memberId와 accessToken.memberId를 동일한지 체킹하는 로직 꼭 필요하다. 
+  - 글 작성 시, request할 시에 서버에서 memberId와 accessToken.memberId를 동일한지 체킹하는 로직 꼭 필요하다.
 
   글 작성 페이지
   - 글 작성 입력 사항은 다음과 같다.
-    - 제목
-    - 공유할 관광지 : 이름, 주소가 저장됨.
+    - 제목 (o)
+    - 공유할 관광지 : 이름, 주소가 저장됨. (o)
       - '등록' 버튼 클릭시, 서브 페이지로 이동한다.
       - 서브 페이지 내부에는 시/도, 구/군, 카테고리, 키워드 검색이 존재한다.
       - 사용자의 경우 마커 클릭 시, 마커위에 창이 뜨며 해당 창 내에서 '등록'을 누를 시에 하단 입력창에 해당 여행지의 이름과 주소가 표시된다.
@@ -87,12 +140,17 @@ import { writeArticle, modifyArticle, getArticle } from "@/api/board";
 /*
 
 공유(또는 공지사항) 에서 글을 작성하고 등록을 보낼 때, 공지사항(또는 공유)로 request하도록 f12키를 통한 수정을 한다면?
-- 
+-
 */
 export default {
   name: "BoardInputItem",
   data() {
     return {
+      isModalOpen: false,
+      selectedModalAttraction: {
+        id: 0,
+        title: "",
+      },
       boardId: {
         value: 0,
         valid: true,
@@ -109,8 +167,19 @@ export default {
         value: "",
         valid: null,
       },
-      boardAttractionInfoId: {
-        value: "",
+      boardAttractionInfo: {
+        value: {
+          contentId: "",
+          title: "",
+        },
+        valid: null,
+      },
+      boardImage: {
+        value: [],
+        valid: null,
+      },
+      boardImageUrl: {
+        value: [],
         valid: null,
       },
     };
@@ -118,62 +187,73 @@ export default {
   props: {
     type: { type: String },
   },
+  computed: {
+    isValidModalAttraction() {
+      const { contentId = 0, title = "" } = this.selectedModalAttraction;
+      return contentId === 0 || title.length === 0;
+    },
+  },
   created() {
     if (this.type === "modify") {
-      let param = this.$route.params.articleno;
+      let param = this.$route.params.boardId;
       getArticle(
         param,
         ({ data }) => {
-          // this.article.articleno = data.article.articleno;
-          // this.article.userid = data.article.userid;
-          // this.article.subject = data.article.subject;
-          // this.article.content = data.article.content;
-          this.article = data;
+          this.boardId.value = data.boardId;
+          this.boardWriterId.value = data.boardWriterId;
+          this.boardTitle.value = data.boardTitle;
+          this.boardContent.value = data.boardContent;
+          this.boardAttractionInfoId.value = data.boardAttractionInfoId;
+          this.boardImage.value = data.boardImage;
         },
         (error) => {
-          console.log(error);
+          switch (error.response.status) {
+            case "500":
+              this.$router.push({
+                name: "error",
+                params: {
+                  msg: "서버 상태 이상",
+                },
+              });
+          }
         }
       );
-      this.isUserid = true;
     }
   },
   methods: {
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+    },
     onSubmit(event) {
       event.preventDefault();
-
       let err = true;
       let msg = "";
-      !this.article.userid &&
-        ((msg = "작성자 입력해주세요"),
-        (err = false),
-        this.$refs.userid.focus());
-      err &&
-        !this.article.subject &&
-        ((msg = "제목 입력해주세요"),
-        (err = false),
-        this.$refs.subject.focus());
-      err &&
-        !this.article.content &&
-        ((msg = "내용 입력해주세요"),
-        (err = false),
-        this.$refs.content.focus());
-
+      if (err && !this.boardTitle.value) {
+        msg = "제목을 입력해주세요";
+        err = false;
+        this.$refs.boardTitle.focus();
+      }
+      if (err && !this.boardContent.value) {
+        msg = "내용을 입력해주세요";
+        err = false;
+        this.$refs.boardTitle.focus();
+      }
       if (!err) alert(msg);
       else
         this.type === "register" ? this.registArticle() : this.modifyArticle();
     },
     onReset(event) {
       event.preventDefault();
-      this.article.articleno = 0;
-      this.article.subject = "";
-      this.article.content = "";
       this.moveList();
     },
     registArticle() {
       let param = {
-        userid: this.article.userid,
-        subject: this.article.subject,
-        content: this.article.content,
+        boardWriterId: this.boardWriterId.value,
+        boardTitle: this.boardTitle.value,
+        boardContent: this.boardContent.value,
       };
       writeArticle(
         param,
@@ -192,10 +272,10 @@ export default {
     },
     modifyArticle() {
       let param = {
-        articleno: this.article.articleno,
-        userid: this.article.userid,
-        subject: this.article.subject,
-        content: this.article.content,
+        boardId: this.boardId.value,
+        boardWriterId: this.boardWriterId.value,
+        boardTitle: this.boardTitle.value,
+        boardContent: this.boardContent.value,
       };
       modifyArticle(
         param,
@@ -216,11 +296,32 @@ export default {
     changeImgFile() {
       return;
     },
+    registModalAttraction(position) {
+      this.selectedModalAttraction = {
+        contentId: position.contentId,
+        title: position.title,
+      };
+    },
+    registAttraction() {
+      //최종 등록한 여행지를 넣어주고, selectedModalAttraction을 초기화해준다.
+      this.boardAttractionInfo.value = { ...this.selectedModalAttraction };
+      this.selectedModalAttraction = {
+        contentId: "",
+        title: "",
+      };
+      this.closeModal();
+    },
     moveList() {
       this.$router.push({ name: "boardlist" });
     },
   },
+  components: { AppDestinationInfo },
 };
 </script>
 
-<style></style>
+<style>
+.custom-popup {
+  width: 800px;
+  height: 800px;
+}
+</style>
