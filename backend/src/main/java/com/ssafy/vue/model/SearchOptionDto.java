2303576a7f05f0f6e2ea2beb8feaf2c58b27f36e
박지영 <pjy7814@naver.com>
2023-05-25 11:48:56 +0900

    fix: 시도 관련 DTO 생성자

--- backend/src/main/java/com/ssafy/vue/model/SearchOptionDto.java
+++ backend/src/main/java/com/ssafy/vue/model/SearchOptionDto.java
@@ -7,13 +7,13 @@ public class SearchOptionDto {
 	private String sidoCode, gugunCode, contentTypeId;
 	private String searchQuery;
 
-
+// 검색조건이 null이어도 검색이 가능함
 	public SearchOptionDto(String sidoCode, String gugunCode, String contentTypeId, String searchQuery) {
 		super();
-		this.sidoCode = sidoCode;
-		this.gugunCode = gugunCode;
-		this.contentTypeId = contentTypeId;
-		this.searchQuery = searchQuery;
+		setSidoCode(sidoCode);
+		setGugunCode(gugunCode);
+		setContentTypeId(contentTypeId);
+		setSearchQuery(searchQuery);
 	}
 	
 	public String getSearchQuery() {
@@ -30,7 +30,7 @@ public class SearchOptionDto {
 		return sidoCode;
 	}
 
-	public void setSidoCode(String sidoCode) throws MyException {
+	public void setSidoCode(String sidoCode) {
 //		if (sidoCode < 0) {
 //			throw new MyException("올바른 입력을 해주세요.");
 //		}
@@ -41,7 +41,7 @@ public class SearchOptionDto {
 		return gugunCode;
 	}
 
-	public void setGugunCode(String gugunCode) throws MyException {
+	public void setGugunCode(String gugunCode) {
 //		if (sidoCode < 0) {
 //			throw new MyException("올바른 입력을 해주세요.");
 //		}
@@ -52,7 +52,7 @@ public class SearchOptionDto {
 		return contentTypeId;
 	}
 
-	public void setContentTypeId(String contentTypeId) throws MyException {
+	public void setContentTypeId(String contentTypeId) {
 //		if (contentTypeId < 0) {
 //			throw new MyException("올바른 입력을 해주세요.");
 //		}
